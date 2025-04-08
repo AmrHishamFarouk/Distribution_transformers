5e74f5e9dffd83b03409342d32a9cb148e344112
AmrHishamFarouk <hishamamr121@gmail.com>
2025-04-08 23:21:14 +0200

    Update specsSlice.js

--- src/database/specsSlice.js
+++ src/database/specsSlice.js
@@ -1,45 +1,49 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    Ratedpower: 0.0, // Float
-    HV: 0, // Float
-    LV: 0.4, // Float
-    Po: 0.0, // Float
-    Pcc: 0.0, // Float
-    Z: 0.0, // Float
-    B: 0.0, // Float
-    F: 0.0, // Float
+  Ratedpower: 0.0, // Float
+  HV: 0, // kV
+  LV: 0.4, // kV
+  Po: 0.0, // Float
+  Pcc: 0.0, // Float
+  Z: 0.0, // Float
+  B: 0.0, // Float
+  F: 0.0, // Float
+  Δironcore: 0.0,
 };
 
 const specsSlice = createSlice({
-    name: 'specs',
-    initialState,
-    reducers: {
-        setRatedpower: (state, action) => {
-            state.Ratedpower = action.payload;
-        },
-        setHV: (state, action) => {
-            state.HV = action.payload;
-        },
-        setLV: (state, action) => {
-            state.LV = action.payload;
-        },
-        setPo: (state, action) => {
-            state.Po = action.payload;
-        },
-        setPcc: (state, action) => {
-            state.Pcc = action.payload;
-        },
-        setZ: (state, action) => {
-            state.Z = action.payload;
-        },
-        setB: (state, action) => {
-            state.B = action.payload;
-        },
-        setF: (state, action) => {
-            state.F = action.payload;
-        },
+  name: 'specs',
+  initialState,
+  reducers: {
+    setRatedpower: (state, action) => {
+      state.Ratedpower = action.payload;
     },
+    setHV: (state, action) => {
+      state.HV = action.payload;
+    },
+    setLV: (state, action) => {
+      state.LV = action.payload;
+    },
+    setPo: (state, action) => {
+      state.Po = action.payload;
+    },
+    setPcc: (state, action) => {
+      state.Pcc = action.payload;
+    },
+    setZ: (state, action) => {
+      state.Z = action.payload;
+    },
+    setB: (state, action) => {
+      state.B = action.payload;
+    },
+    setF: (state, action) => {
+      state.F = action.payload;
+    },
+    setΔironcore: (state, action) => {
+      state.Δironcore = action.payload;
+    },
+  },
 });
 
 // Selectors
@@ -51,17 +55,19 @@ export const selectPcc = (state) => state.specs.Pcc;
 export const selectZ = (state) => state.specs.Z;
 export const selectB = (state) => state.specs.B;
 export const selectF = (state) => state.specs.F;
+export const selectΔironcore = (state) => state.specs.Δironcore;
 
 // Actions
-export const { 
-    setRatedpower, 
-    setHV, 
-    setLV, 
-    setPo, 
-    setPcc, 
-    setZ, 
-    setB, 
-    setF, 
+export const {
+  setRatedpower,
+  setHV,
+  setLV,
+  setPo,
+  setPcc,
+  setZ,
+  setB,
+  setF,
+  setΔironcore,
 } = specsSlice.actions;
 
 // Reducer
