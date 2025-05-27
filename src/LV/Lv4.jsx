649f2cb331330ea6b3eb1a36d12cc5b05c0f8eda
AmrHishamFarouk <hishamamr121@gmail.com>
2025-05-27 11:20:51 +0300

    Update Lv4.jsx

--- src/LV/Lv4.jsx
+++ src/LV/Lv4.jsx
@@ -1,51 +1,87 @@
 function Lv4(){
-    // Dmean equation
-    // Dmeanlv = (a + b) * ((3 * Math.pow(a - b, 2)) / (Math.pow(a + b, 2) * Math.sqrt((-3 * Math.pow(a - b, 2)) / Math.pow(a + b, 2) + 4) + 10)) + 1;
-    // Radiallv = ((thicknessofwire*layerslv)+(layerslv*noins*insthickness)+(numcooling*coolingthickness))*1.035
-    // if(wire == 'Foil'){
-    //     Axiallv = Radiallv + (2*bars) + (2*3*0.35)*1.035
-    // }else if(wire == 'Flat'){
-    //     Axiallv = Radiallv + (2*turnthickness) + (2*3*0.35)*1.035
-    // }else{
+import React, { useState } from 'react';
 
-    // };
+import { useSelector, useDispatch } from 'react-redux';
+import { setLV, selectLV } from './../database/lvSlice';
+import { setSpec, selectSpec} from './../database/specsSlice';
+
+
+let thickradial = ((Layerslv*Turnthicknesslv)+(Layerslv*  Noinsl*InsulationPaperThicknesslv)+(Nocollingductlv*Coolingductthickness))*1.035;
+if(Wiretypelv== 'foil'){
+  let Thickaxiallv = thickradial+(2*Barslv)+((2*3*0.35)*1.035);
+}else if(Wiretypelv== 'flat'){
+let Thickaxiallv = thickradial + (2*Turnthicknesslv)+((2*3*0.35)*1.035)}
+else if(Wiretypelv== 'round'){
+    Thickaxiallv = thickradial;
+}
+ 
+Фexternalradial = Фinternalradial + (2*thickradial);
+Фexternalaxial =  Фinternalaxial + (2*Thickaxiallv) - 2bars or wires;
+ 
+ 
+ a = ( Фexternalaxial - Thickaxiallv )/2;
+ b = ( Фexternalradial - thickradial )/2;
+ 
+    // Dmean equation
+     let Dmeanlv = (a + b) * ((3 * Math.pow(a - b, 2)) / (Math.pow(a + b, 2) * Math.sqrt((-3 * Math.pow(a - b, 2)) / Math.pow(a + b, 2) + 4) + 10)) + 1;
     
-    
+    dispatch(setLV({ key: 'Dmeanlv', value: Dmeanlv }));
+    let Dmlv = Фinternalradial + thickradial;
+        
+    dispatch(setLV({ key: 'Dmlv', value: Dmlv }));
+    //need adjustments according to the wire type
+     singlephaseCoppermasslv = (22/7)*Dmeanlv*Nph*0.001*Csalv*8.9*0.001;
+     coppermass = 3*singlephaseCoppermasslv;
+     
+     dispatch(setLV({ key: 'Coppermasslv', value: coppermass }));
+     
+     Connectionlv = Iphlv/200;
+     //adjust this equation
+     Eddylv = (Wirethicknesslv^4)*(Nph^2)*0.001;
+     Pcc75lvtemp = 2.097*0.00001*( ((22/7)*Dmeanlv*Nph)/Csalv)*3*Iphlv;
+     let pccConn= (0.01*Connectionlv);
+     let pccEddy= (0.01*Eddylv);
+     
+     pccAdditioned = Pcc75lvtemp+pccConn+pccEddy;
+     Pcc75lv = pccAdditioned * (0.01*Maj);
+     Pcc95lv = Pcc75lv*1.064;
+     
+     
+     
+     Rph75lvtemp = 2.097*0.00001*( ((22/7)*Dmeanlv*Nph)/Csalv);
+     Rph75lv= Rph75lvtemp * (0.01*Maj);
+     Rph95lv= Rph75lv*1.064;
+     Rbettwoterminalslv=Rph95lv*2;
+     Rph20lv=  Rph95lv  *1.2947;
+     
+     
+     
+
     return(
-        <center><h1>Lv4</h1></center>
-        // <>
-    // <h1>Thickness LV</h1>
+    <>
+        <h1>Thickness LV</h1>
     
-    // <div>
-    //     <strong>Radial LV:</strong>
-    //     <p>4 × V / x 10⁻³</p>
-    // </div>
+    <div>
+        <strong>Radial LV: {thickradial}</strong>
+    </div>
     
-    // <div>
-    //     <strong>Axial LV:</strong>
-    //     <p>20 × 1.15 × 0.35 / (n × 0.13)</p>
-    // </div>
+     <div>
+        <strong>Axial LV: { Thickaxiallv }</strong>
+    </div>
+     <div>
+         <p>Φ LV = {Фinternalradial}/{Фinternalaxial} Ξ {Фexternalradial} /{Фexternalaxial}</p>
+    </div>
     
-    // <div>
-    //     <p>Φ LV = / Ξ /</p>
-    // </div>
+    <div>
+        <p>alv =  {a}   ,b lv =   {b}</p>
+        <p>Dmean = {Dmeanlv} </p>
+    </div>
     
-    // <div>
-    //     <p>a lv = b lv = d mean lv =</p>
-    // </div>
-    
-    // <div>
-    //     <strong>For Ux:</strong>
-    //     <p>d m lv =</p>
-    // </div>
-    
-    // <div>
-    //     <strong>Copper Mass:</strong>
-    //     <p>Mass 1ph</p>
-    //     <p>20 × 1.15 × 0.35 / (n × 0.13) =</p>
-    //     <p>3 ph =</p>
-    // </div> 
-    // </>
+    <div>
+    <strong>For Ux:</strong>
+    <p>Dmlv = {Dmlv}</p>
+    </div>
+    </>
   )
 }
 export default Lv4;
