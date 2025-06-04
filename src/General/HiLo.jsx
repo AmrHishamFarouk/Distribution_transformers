dd6c0502a80f3d900b23da4bd8b67f0d317990ab
AmrHishamFarouk <hishamamr121@gmail.com>
2025-06-04 15:06:57 +0300

    hilo adjusted

--- src/General/HiLo.jsx
+++ src/General/HiLo.jsx
@@ -1,33 +1,52 @@
 import { setHilo, selectHilo } from '../database/hiloSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { setLV, selectLV } from './../database/lvSlice';
 
 function HiLo(){
     const dispatch = useDispatch();
-
+    const Фexternalradiallv =  useSelector((state) => selectLV(state, 'Фexternalradial'));
+    const Фexternalaxiallv =  useSelector((state) => selectLV(state, 'Фexternalaxial'));
     const Thickhilo =  useSelector((state) => selectHilo(state, 'Thickhilo'));
-    dispatch(setHilo({ key: 'Фinternalradial', value: Фinternalradial}));
-    // Canallvhilo: 0.0,
-    // Psphilo: 0.0,
-    // Canalhvhilo: 0.0,
-    // Thickhilo: 0.0,
-    // Radialexternalhilo: 0.0,
-    // Axialexternalhilo: 0.4,
-    // Dmeanhilo: 0.0,
-    // Dmhilo: 0.0,
+    const Radialexternalhilo =  useSelector((state) => selectHilo(state, 'Radialexternalhilo'));
+    const Axialexternalhilo =  useSelector((state) => selectHilo(state, 'Axialexternalhilo'));
+    const Dmhilo =  useSelector((state) => selectHilo(state, 'Dmhilo'));
+
+
+    function Changehilo(sign){
+          if (sign === '+' && Thickhilo < 16.5){
+            dispatch(setHilo({ key: 'Thickhilo', value: Thickhilo + 2}));
+            return ;
+          } 
+          if (sign === '-' && Thickhilo > 8.5){
+            dispatch(setHilo({ key: 'Thickhilo', value: Thickhilo - 2}));
+            return ;
+          } 
+          return Thickhilo;    
+      };
+
+      useEffect(() => {
+        let Radialexternalhilo = Фexternalradiallv+((2*Thickhilo)+(2*0.35))
+
+        dispatch(setHilo({ key: 'Radialexternalhilo', value: Thickhilo + 2}));
+        dispatch(setHilo({ key: 'Axialexternalhilo', value: Thickhilo + 2}));
+  
+
+    }, [Thickhilo]);
 
     return(
         <>
         <h1>Thickness HI-LO : {Thickhilo}</h1>
-
     
      <div>
-         <p>Φ HI-LO = {Фinternalradial}/{Фinternalaxial} Ξ {Фexternalradial} /{Фexternalaxial}</p>
+         <p>Φ HI-LO = {Фexternalradiallv}/{Фexternalaxiallv} Ξ {Radialexternalhilo} /{Axialexternalhilo}</p>
     </div>
     
      <div>
      <strong>For Ux:</strong>
      <p>Dmtr = {Dmlv}</p>
      </div>
+     <button onClick={() => Changehilo('+')}> increase Thickness </button>
+     <button onClick={() => Changehilo('-')}> decrease Thickness </button>
         </>
     )
 }
