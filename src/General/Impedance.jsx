725b3745c4812511ca36b034a2f00f281a2b12b9
AMR hisham <hishamamr121@gmail.com>
2025-09-29 19:13:39 +0300

    impedance and forces

--- src/General/Impedance.jsx
+++ src/General/Impedance.jsx
@@ -1,15 +1,16 @@
 import { setLV, selectLV } from './../database/lvSlice';
 import { setHV, selectHV } from './../database/lvSlice';
+import { setHilo, selectHilo } from './../database/hiloSlice';
+import { setSpec, selectSpec } from './../database/lvSlice';
 
 
 function Impedance(){
 
-const Ratedpower = useSelector((state) => selectHV(state, 'Ratedpower'));
-const LV = useSelector((state) => selectHV(state, 'LV'));
-const Totalloadlosses
-//the total losses is not found in the slices
-const F = useSelector((state) => selectHV(state, 'Ratedpower'));
-const Maj = useSelector((state) => selectHV(state, 'Maj'));
+const Ratedpower = useSelector((state) => selectSpec(state, 'Ratedpower'));
+const LV = useSelector((state) => selectSpec(state, 'LV'));
+const F = useSelector((state) => selectSpec(state, 'Ratedpower'));
+const Maj = useSelector((state) => selectSpec(state, 'Maj'));
+
 const Thicklvradial = useSelector((state) => selectLV(state, 'Thickradiallv'));
 const Dmlv = useSelector((state) => selectLV(state, 'Dmlv'));
 const δlv = useSelector((state) => selectLV(state, 'δlv'));
@@ -24,19 +25,21 @@ const Ilv = useSelector((state) => selectLV(state, 'Iphlv'));
 //we need to make a num variable to identify if it is power or normal distrebution
 
 //hilo variables
-const Dmhilo = useSelector((state) => selectLV(state, 'Dmhilo'));
-const Thickhilo = useSelector((state) => selectLV(state, 'Thickhilo'));
+const Dmtr = useSelector((state) => selectHilo(state, 'Dmhilo'));
+const Hilothickness = useSelector((state) => selectHilo(state, 'Thickhilo'));
+// const Rods = useSelector((state) => selectHilo(state, 'Thickhilo'));
 
 //we need to add distance between rods in the hilo slice
-const Thickradialhv = useSelector((state) => selectHV(state, 'Thickradialhv'));
+const Thickhv = useSelector((state) => selectHV(state, 'Thickradialhv'));
 const Helechv = useSelector((state) => selectHV(state, 'Helechv'));
 const Dmhv = useSelector((state) => selectHV(state, 'Dmhv'));
 const turns = useSelector((state) => selectHV(state, 'Turnshv'));
+const Turnsatcentertap = turns[2];
 const Iphhv = useSelector((state) => selectHV(state, 'Iphhv'));
 const δhvpos7 = useSelector((state) => selectHV(state, 'δhvpos7'));
 const Dmeanhv = useSelector((state) => selectHV(state, 'Dmeanhv'));
-const Csahv = useSelector((state) => selectHV(state, 'Csahv'));
-
+const Δhv = useSelector((state) => selectHV(state, 'Csahv'));
+// const Totalloadlosses = useSelector((state) => selectHV(state, 'Csahv'));
 
       let Kf = 1 - ((Thicklvradial + Hilothickness + Thickhv) / ((22 / 7) * Helechv));
       let Ur = Totalloadlosses / (Ratedpower * 10);
@@ -58,6 +61,7 @@ const Csahv = useSelector((state) => selectHV(state, 'Csahv'));
       let tempcoillv = 105 + (8.2 * Math.pow(((100 / Ucc) * δlv), 2) * Tsc * 0.001);
       let tempcoilhv = 105 + (8.2 * Math.pow(((100 / Ucc) * δhvpos7), 2) * Tsc * 0.001);
       
+
       // Mechanical Calculations
       let Isc = (100 / Ucc) * Ilv * Math.sqrt(2) * (1 + Math.exp((-22 / 7) * (Ur / Ux)));
       let Ec = (2 * Math.pow((22 / 7), 2) * Math.pow((Nphlv * Isc), 2) * (Dmtr * 0.001) *
@@ -99,91 +103,31 @@ const Csahv = useSelector((state) => selectHV(state, 'Csahv'));
       
 return(
         <>
-        <h1>impedance</h1>
+            {Kf}
+            {Ur}
+            {Uxtemp}
+            {Ux}
+            {Ucc}
+            {voltagedrop08}
+            {voltagedropunity}
+            {voltagelv08}
+            {Tsclv}
+            {Tschv} 
+            {Tsc}
+            {tempcoillv}
+            {tempcoilhv}
+
+            {Isc}
+            {Ec}
+            {d} 
+            {Fd}
+
+            {σcttotal}
+            {σftlv}
+            {σexhv}
+
+
         </>
     )
 }
 export default Impedance;
-
-
-
-
-
-//     <!-- First Group -->
-//     <div class="form-group">
-//       <label for="Ratedpower">Rated Power(kVA):</label>
-
-//       <label for="LV"> LV(do not write it in kV):</label>
-
-//       <label for="Totalloadlosses">Total Load Losses(at 95):</label>
-
-//       <label for="F">Frequency (HZ):</label>
-
-//       <label for="Maj">Maj(%):</label>
-
-//       <label for="Thicklvradial">Thick LV Radial(mm):</label>
-
-//       <label for="Dmlv">Dmlv(mm):</label>
-
-//       <label for="δlv">δlv(A/mm²):</label>
-
-//       <label for="Dmeanlv">Dmeanlv(mm):</label>
-
-//       <label for="Nphlv">Nphlv:</label>
-
-//       <label for="Δlv">Δlv(sum of all wires):</label>
-
-//       <label for="Wirelengthlv">Wirelengthlv(mm):</label>
-
-//       <label for="Wirethicknesslv">Wirethicknesslv(mm):</label>
-
-//       <label for="Heleclv">Heleclv(mm):</label>
-
-//       <label for="Hmechlv">Hmechlv(mm):</label>
-
-//       <label for="Ilv">Ilv(A):</label>
-
-//       <label for="num">for power(12),for distrebution(6):</label>
-//     </div>
-
-//     <!-- Second Group -->
-//     <div class="form-group">
-//       <h2>Intermediate Parameters</h2>
-//       <label for="Dmtr">Dmtr(mm):</label>
-//       <input type="number" id="Dmtr">
-
-//       <label for="Hilothickness">Hilothickness(mm):</label>
-//       <input type="number" id="Hilothickness">
-
-//       <label for="ٌRods">distance between PSP sticks(mm):</label>
-//       <input type="number" id="Rods" value="20">
-//     </div>
-
-//     <!-- Third Group -->
-//     <div class="form-group">
-//       <h2>HV Parameters</h2>
-//       <label for="Thickhv">Thickhv(mm):</label>
-//       <input type="number" id="Thickhv">
-
-//       <label for="Helechv">Helechv(mm):</label>
-//       <input type="number" id="Helechv">
-
-//       <label for="Dmhv">Dmhv(mm):</label>
-//       <input type="number" id="Dmhv">
-
-//       <label for="Turnsatcentertap">Turns at Center Tap:</label>
-//       <input type="number" id="Turnsatcentertap">
-
-//       <label for="Iphhv">Iphhv(A):</label>
-//       <input type="number" id="Iphhv">
-
-//       <label for="δhvpos7">δhvpos7(A/mm²):</label>
-//       <input type="number" id="δhvpos7">
-
-//       <label for="Dmeanhv">Dmeanhv(mm):</label>
-//       <input type="number" id="Dmeanhv">
-
-//       <label for="Δhv">Δhv(sum of all wires in mm²):</label>
-//       <input type="number" id="Δhv">
-//     </div>
-//   </div>
