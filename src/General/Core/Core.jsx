ef531ae82679101bfd71925660c9737e4f8eb743
AmrHishamFarouk <hishamamr121@gmail.com>
2025-12-04 10:10:21 +0200

    Update Core.jsx

--- src/General/Core/Core.jsx
+++ src/General/Core/Core.jsx
@@ -6,20 +6,18 @@ import Detailedcore from './Detailedcore';
 import { setGeneral, selectGeneral} from '../../database/generalSlice';
 import { setLV, selectLV } from '../../database/lvSlice';
 import './../../CSS/General/Core.css'
+import Calc1 from './../../Calc1'
+
 function Core(){
     const dispatch = useDispatch();
-
     let [simpleCore, setsimpleCore] = useState(true);
-    
-    
-const Csacore =  useSelector((state) => selectGeneral(state, 'Csacore'));
-const Sticks =  useSelector((state) => selectGeneral(state, 'Sticks'));
-const Cyl =  useSelector((state) => selectGeneral(state, 'Cyl'));
-const Tums =  useSelector((state) => selectGeneral(state, 'Tums'));
-const W1 =  useSelector((state) => selectGeneral(state, 'W1'));
-const Inssteel =  useSelector((state) => selectGeneral(state, 'Inssteel'));
-const Totalstacking =  useSelector((state) => selectGeneral(state, 'Totalstacking'));
-
+  const Csacore =  useSelector((state) => selectGeneral(state, 'Csacore'));
+  const Sticks =  useSelector((state) => selectGeneral(state, 'Sticks'));
+  const Cyl =  useSelector((state) => selectGeneral(state, 'Cyl'));
+  const Tums =  useSelector((state) => selectGeneral(state, 'Tums'));
+  const W1 =  useSelector((state) => selectGeneral(state, 'W1'));
+  const Inssteel =  useSelector((state) => selectGeneral(state, 'Inssteel'));
+  const Totalstacking =  useSelector((state) => selectGeneral(state, 'Totalstacking'));
 
   useEffect(() => {
     const Фinternalradial = W1 + (2*Sticks) +(2*Cyl)+(2*2*Tums)
@@ -36,19 +34,16 @@ const Фinternalaxial =  useSelector((state) => selectGeneral(state, 'Фinternal
 
     return(
     <>
-<div className="core-toggle">
-  <button onClick={() => setsimpleCore(true)}>Simple Core</button>
-  <button onClick={() => setsimpleCore(false)}>Detailed Core</button>
-</div>
+      <Calc1/>
+      <div className="core-toggle">
+        <button onClick={() => setsimpleCore(true)}>Simple Core</button>
+        <button onClick={() => setsimpleCore(false)}>Detailed Core</button>
+      </div>
 
-{simpleCore === true && <Quickcore />}
-{simpleCore === false && <Detailedcore />}
+      {simpleCore === true && <Quickcore />}
+      {simpleCore === false && <Detailedcore />}
 
     </>
     )
 }
 export default Core;
-
-
-
-
